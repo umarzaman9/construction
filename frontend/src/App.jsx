01a1ea108caa7bd2edb07878ad7bd2109551f573
Umer Zaman <umer.zaman@techswivel.com>
2026-06-01 16:18:09 +0500

    added header on Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/frontend/Home';
+import './assets/css/style.scss';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="d-flex justify-content-center align-items-center vh-100">
-        <button className="btn btn-primary">Click here</button>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/components/frontend/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Home = () => {
+    return (
+        <header>
+            <div className="container py-3">
+                <Navbar expand="lg">
+                    <Navbar.Brand href="#home" className="logo"> <span>Rootage</span> Constructions</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
+                            <Nav.Link href="#link" className="nav-link">About Us</Nav.Link>
+                            <Nav.Link href="#link" className="nav-link">About Us</Nav.Link>
+                            <Nav.Link href="#link" className="nav-link">Services</Nav.Link>
+                            <Nav.Link href="#link" className="nav-link">Blogs</Nav.Link>
+                            <Nav.Link href="#link" className="nav-link">Contact Us</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        </header>
+    )
+}
+
+export default Home
