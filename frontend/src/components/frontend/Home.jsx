bbcb06df76aa741fb6848e80e5a9f89a78758475
Umer Zaman <umer.zaman@techswivel.com>
2026-06-04 16:25:52 +0500

    implemented hero section

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -5,25 +5,50 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const Home = () => {
     return (
-        <header>
-            <div className="container py-3">
-                <Navbar expand="lg">
-                    <Navbar.Brand href="#home" className="logo"> <span>Rootage</span> Constructions</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
-                            <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
-                            <Nav.Link href="#link" className="nav-link">About Us</Nav.Link>
-                            <Nav.Link href="#link" className="nav-link">About Us</Nav.Link>
-                            <Nav.Link href="#link" className="nav-link">Services</Nav.Link>
-                            <Nav.Link href="#link" className="nav-link">Blogs</Nav.Link>
-                            <Nav.Link href="#link" className="nav-link">Contact Us</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
-        </header>
+        <>
+            <header>
+                <div className="container py-3">
+                    <Navbar expand="lg">
+                        <Navbar.Brand href="#home" className="logo"> <span>Rootage</span> Constructions</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ms-auto">
+                                <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
+                                <Nav.Link href="#link" className="nav-link">About Us</Nav.Link>
+                                <Nav.Link href="#link" className="nav-link">About Us</Nav.Link>
+                                <Nav.Link href="#link" className="nav-link">Services</Nav.Link>
+                                <Nav.Link href="#link" className="nav-link">Blogs</Nav.Link>
+                                <Nav.Link href="#link" className="nav-link">Contact Us</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
+            </header>
+
+            <main>
+                <section className="section-1" >
+                    <div className="hero d-flex align-items-center">
+                        <div className="container-fluid">
+                            <div className="text-center">
+                                <span>Welcom Amazing Constructions</span>
+                                <h1>Crafting Dreams with precision and excellence.</h1>
+                                <p>We Excel at transforming vision into reality.</p>
+                                <div className="mt-4">
+                                    <a href="#" className="btn btn-primary">Contact Now</a>
+                                    <a href="#" className="btn btn-secondary ms-3">View Projects</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+
+            <footer>
+
+            </footer>
+        </>
     )
+
 }
 
 export default Home
